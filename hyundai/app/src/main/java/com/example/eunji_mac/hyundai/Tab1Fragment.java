5bebb5e13908c00a0dd02348b324f56b39ab18d1
eunji6546 <eunji6546@gmail.com>
2016-08-19 18:49:35 +0900

    first

---
+++ hyundai/app/src/main/java/com/example/eunji_mac/hyundai/Tab1Fragment.java
@@ -0,0 +1,48 @@
+package com.example.eunji_mac.hyundai;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Tab1Fragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Tab1Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Tab1Fragment extends Fragment {
+    public Tab1Fragment() {
+        // Required empty public constructor
+    }
+
+    //I removed static !!!
+    // TODO: Rename and change types and number of parameters
+    public static Tab1Fragment newInstance(Bundle args) {
+        Tab1Fragment fragment = new Tab1Fragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView  = inflater.inflate(R.layout.fragment_tab1, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
